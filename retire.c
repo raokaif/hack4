b94ca8c053aaa79c2d7a520bdbcf23c7626f5deb
raokaif <kaifrao250@gmail.com>
2023-10-22 16:29:31 +0500

    solving mistakes

--- retire.c
+++ retire.c
@@ -4,25 +4,27 @@ int main(int argc, char const *argv[])
     float initialBalance, MonthlyContribution, RateOfReturn, RateOfInflation,inflationAdjusted, interest;
     int year, month;
     printf(" Enter An initial starting balance\n");
-    scanf("%d",&initialBalance);
+    scanf("%f",&initialBalance);
     printf(" Enter A monthly contribution amount\n");
-    scanf("%d",&MonthlyContribution);
+    scanf("%f",&MonthlyContribution);
     printf(" Enter An (average) annual rate of return (on the scale [0,1])\n");
-    scanf("%d",&RateOfReturn);
+    scanf("%f",&RateOfReturn);
     printf(" Enter An (average) annual rate of inflation (on the scale [0,1])\n");
-    scanf("%d",&RateOfInflation);
+    scanf("%f",&RateOfInflation);
     printf(" Enter A number of years until retirement\n");
     scanf("%d",&year);
-    inflationAdjusted = ((1 +RateOfReturn) / (1 + RateOfInflation)) - 1;
-    inflationAdjusted = inflationAdjusted / 12;
+    inflationAdjusted = ((1 +(float)RateOfReturn) / (1 + RateOfInflation)) - 1;
+    inflationAdjusted = (float)inflationAdjusted / 12;
+    interest = inflationAdjusted;
     month = year * 12;
-    for (int i = 0; i <= month; i++)
+    for (int i = 1; i <= month; i++)
     {
-        initialBalance = initialBalance + MonthlyContribution + interest;
-        interest = interest * initialBalance;
-        printf("%d",i);
-        printf("%d",initialBalance);
-        printf("%d",interest);
+        
+        interest = interest * (float)initialBalance;
+        initialBalance = initialBalance + MonthlyContribution + (float)interest;
+        printf("%d\n",i);
+        printf("%.2f\n",initialBalance);
+        printf("%.2f\n",interest);
         
 
         
