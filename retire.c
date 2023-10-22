7d24696a5433bd44fab0836ac8303e63a2a90586
raokaif <kaifrao250@gmail.com>
2023-10-22 17:15:46 +0500

    converting to command line argument

--- retire.c
+++ retire.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
-int main(int argc, char const *argv[])
+#include<stdlib.h>
+int main(int argc, char *argv[])
 {
+    if (argc != 6)
+    {
+        printf("Please provide 6 command line arguments.\n");
+        return 1;
+    }
     float initialBalance, MonthlyContribution, RateOfReturn, RateOfInflation,inflationAdjusted, interest;
     int year, month;
-    printf(" Enter An initial starting balance\n");
-    scanf("%f",&initialBalance);
-    printf(" Enter A monthly contribution amount\n");
-    scanf("%f",&MonthlyContribution);
-    printf(" Enter An (average) annual rate of return (on the scale [0,1])\n");
-    scanf("%f",&RateOfReturn);
-    printf(" Enter An (average) annual rate of inflation (on the scale [0,1])\n");
-    scanf("%f",&RateOfInflation);
-    printf(" Enter A number of years until retirement\n");
-    scanf("%d",&year);
+
+    initialBalance = atof(argv[1]);
+    MonthlyContribution = atof(argv[2]);
+    RateOfReturn = atof(argv[3]);
+    RateOfInflation = atof(argv[4]);
+    year = atoi(argv[5]);
+
     inflationAdjusted = ((1 +(float)RateOfReturn) / (1 + RateOfInflation)) - 1;
     inflationAdjusted = (float)inflationAdjusted / 12;
     interest = inflationAdjusted;
     month = year * 12;
+
     printf("Month\tInterest\tBalance\n");
+
     for (int i = 1; i <= month; i++)
     {
         interest = inflationAdjusted * (float)initialBalance;
