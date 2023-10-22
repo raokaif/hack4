5c2b582d77a53961d0f81ccbb2c66bd48e49f5cb
raokaif <kaifrao250@gmail.com>
2023-10-22 15:14:16 +0500

    scanf for inputs

--- retire.c
+++ retire.c
@@ -3,5 +3,15 @@ int main(int argc, char const *argv[])
 {
     float initialBalance, MonthlyContribution, RateOfReturn, RateOfInflation;
     int year;
+    printf(" Enter An initial starting balance\n");
+    scanf("%d",&initialBalance);
+    printf(" Enter A monthly contribution amount\n");
+    scanf("%d",&MonthlyContribution);
+    printf(" Enter An (average) annual rate of return (on the scale [0,1])\n");
+    scanf("%d",&RateOfReturn);
+    printf(" Enter An (average) annual rate of inflation (on the scale [0,1])\n");
+    scanf("%d",&RateOfInflation);
+    printf(" Enter A number of years until retirement\n");
+    scanf("%d",&year);
     return 0;
 }
